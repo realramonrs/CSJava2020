d8f46648a8f96ac5aa3fe36f4bf22c95400ca33b
ramonrsgithub <celmon1@gmail.com>
2020-10-03 07:43:45 +0200

    Update Ejercicio5.java

--- 02 - Tipos de datos/ejercicios/Ejercicio5.java
+++ 02 - Tipos de datos/ejercicios/Ejercicio5.java
@@ -4,7 +4,11 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		float precio,precioRebajado;
+		precio = 352;
+		precioRebajado = precio - precio*0.15;
+		System.out.println("Precio original: " + precio);
+		System.out.println("Precio rebajado: " + precioRebajado);
 		
 	}
 
