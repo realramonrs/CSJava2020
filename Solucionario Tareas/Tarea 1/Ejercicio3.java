eba8f6d2ac2c0a4f6c73a2cdf896be0d5b2968dd
RamonRS <celmon1@gmail.com>
2020-10-21 15:52:29 +0200

    Update Ejercicio3.java

--- Solucionario Tareas/Tarea 1/Ejercicio3.java
+++ Solucionario Tareas/Tarea 1/Ejercicio3.java
@@ -16,30 +16,20 @@ public class Ejercicio3 {
 
 		String frase = JOptionPane.showInputDialog("Escriba una frase");
 		
-		//a. Número de palabras introducidas.
-			System.out.println("Número de palabras introducidas: " + frase.split(" ").length);
+		//a. NÃºmero de palabras introducidas.
+			System.out.println("NÃºmero de palabras introducidas: " + frase.split(" ").length);
 			
-		//b. Número de caracteres no espacios.
+		//b. NÃºmero de caracteres no espacios.
 			int caracteres = frase.length();
 			int espacios = frase.split(" ").length - 1;
-			System.out.println("Número de caracteres no espacios: " + (caracteres - espacios));
+			System.out.println("NÃºmero de caracteres no espacios: " + (caracteres - espacios));
 			
-		//c. Primera letra de las palabras introducidas.
+		//c. Comprobar si el primer caracter es un dÃ­gito.
 			
-			String palabras[] = frase.split(" ");
-			//Como no sabemos bucles vamos a suponer que
-			//la frase tiene 3 palabras
-			char letraPalabra0 = palabras[0].charAt(0);
-			char letraPalabra1 = palabras[1].charAt(0);
-			char letraPalabra2 = palabras[2].charAt(0);
+			System.out.println("El primer caracter es un dÃ­gito?: " + Character.isDigit(frase.charAt(0));
 			
-			System.out.println("Primeras letras ");
-			System.out.println(letraPalabra0);
-			System.out.println(letraPalabra1);
-			System.out.println(letraPalabra2);
-			
-		//d. Posición del primer espacio.
-			System.out.println("Posición del primer espacio:"  + frase.indexOf(' '));
+		//d. PosiciÃ³n del primer espacio.
+			System.out.println("PosiciÃ³n del primer espacio:"  + frase.indexOf(' '));
 	}
 
 }
